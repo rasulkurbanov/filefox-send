58675dbbcd3cba5222580391c466e1d6c6e79aac
rasulkurbanov <rasuljohn139@gmail.com>
2021-03-12 23:11:16 +0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import "./App.css";
+
+
+
 
 function App() {
+  const [ files, setFiles ] = useState(null)
+
+
+  const onChangeHandler = (e) => {
+    // console.log(e.target.files[0])
+    setFiles(e.target.files[0])
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row">
+        <div className="col-md-6">
+          <form method="post" action="#" id="#">
+            <div className="form-group files color">
+              <label>Upload Your File </label>
+              <input type="file" className="form-control" multiple="" onChange={onChangeHandler}/>
+              <button type="button" className="btn btn-success btn-block">Upload</button> 
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
